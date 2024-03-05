3978fc6f3be32aa7c450b18e0dd79213a6138c82
Clmt2707 <clement.deboeuf@gmail.com>
2024-03-05 10:25:40 +0100

    ajout fonction login

--- controllers/users.js
+++ controllers/users.js
@@ -18,3 +18,28 @@ exports.signup = (req, res, next) => {
         })
         .catch(error => res.status(500).json({ error }));
 };
+
+exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if (!user) {
+                res.status(401).json({ message: 'Paire login/mdp incorrecte'});
+            }
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mdp incorrecte !'});
+                    } 
+                    res.status(200).json({
+                        userId: user._id,
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+};
